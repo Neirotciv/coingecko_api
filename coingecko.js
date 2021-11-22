7a580a36ce2ceb83da7fe0a5b2754987f3169a22
Neirotciv <tanacetifolia@gmail.com>
2021-11-22 18:47:06 +0100

    ajout de la requete markets

--- coingecko.js
+++ coingecko.js
@@ -1,19 +1,19 @@
 const pingUrl = 'https://api.coingecko.com/api/v3/ping';
 const coinListUrl = 'https://api.coingecko.com/api/v3/coins/list';
+const coinMarketByIdUrl = 'https://api.coingecko.com/api/v3/coins/markets/';
 
 const connectButton = document.getElementById('connect-button');
-let isConnected = false;
-
 connectButton.addEventListener('click', async function () {
     await fetch(pingUrl)
         .then(function (response) {
             let status = document.getElementById('status');
             if (response.status == 200) { 
-                loadCoinList();
+                // loadCoinList();
             }
             status.innerText = 'Status : ' + response.status;
             return response.json();
-        }).then(function (data) {
+        })
+        .then(function (data) {
             let ping = document.getElementById('gecko-says');
             ping.innerText = data['gecko_says'];
         });
@@ -25,8 +25,6 @@ const loadCoinList = function () {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data);
-            // get coin list div
             let coinList = document.getElementById('coin-list');
             data.forEach(element => {
                 // id, symbol, name
@@ -37,6 +35,25 @@ const loadCoinList = function () {
         })
 }
 
-if (isConnected) {
-    loadCoinList();
-}
+const searchButton = document.getElementById('search-button');
+searchButton.addEventListener('click', async function() {
+    let coinId = document.getElementById('coin-id').value;
+    let vsCurrency = '?vs_currency=' + 'usd';
+    let ids = '&ids=' + coinId;
+    let data = await fetch(coinMarketByIdUrl + vsCurrency + ids)
+        .then(function (response) {
+            console.log(response.status);
+            return response.json();
+        })
+        .then(function (data) {
+            let imageTag = document.getElementById('ids-image');
+            imageTag.src = data[0]['image'];
+
+            let marketInfosDiv = document.getElementById('market-infos')
+            let name = document.createElement('h3');
+            name.innerText = data[0]['name']
+
+            marketInfosDiv.appendChild(name);
+            return data[0];
+        });
+});
